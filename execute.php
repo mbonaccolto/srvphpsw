0f9f6202ecf3c541df88dc678ef160ac396cac25
mbonaccolto <53046873+mbonaccolto@users.noreply.github.com>
2019-08-14 14:17:22 +0200

    Update execute.php
    test con risposte casuali

--- execute.php
+++ execute.php
@@ -19,6 +19,41 @@ $text = isset($message['text']) ? $message['text'] : "";
 $text = trim($text);
 $text = strtolower($text);
 
+$answers = array(
+  "Per quanto posso vedere, sì",
+  "È certo",
+  "È decisamente così",
+  "Molto probabilmente",
+  "Le prospettive sono buone",
+  "I segni indicano di sì",
+  "Senza alcun dubbio",
+  "Sì",
+  "Sì",
+  "Ci puoi contare",
+  "È difficile rispondere, prova di nuovo",
+  "Rifai la domanda più tardi",
+  "Meglio non risponderti adesso",
+  "Non posso predirlo ora",
+  "Concentrati e rifai la domanda",
+  "Non ci contare",
+  "La mia risposta è no",
+  "Le mie fonti dicono di no",
+  "Le prospettive non sono buone",
+  "Molto incerto"
+);
+
+if (!$text) {
+  // Verificare che l'ultimo carattere sia un punto di domanda
+    if ($text[sizeof($text) - 1] == '?') {
+    // Da qui mandi la risposta
+    $answer = $answers[rand(0, sizeof($answers)-1)];
+    } 
+    else {
+      // Da qui gli mandi "ehy fammi una domanda"
+      $answer = "Per poterti rispondere, mi devi fare una domanda...";
+    }
+}
+
 header("Content-Type: application/json");
 $parameters = array('chat_id' => $chatId, "text" => $text);
 $parameters["method"] = "sendMessage";
