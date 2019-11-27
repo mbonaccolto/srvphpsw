104aae7a792c24ab60e1557ce5b3aa76f73b88b9
mbonaccolto <53046873+mbonaccolto@users.noreply.github.com>
2019-11-27 11:25:36 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -36,7 +36,7 @@ if ($text == '/relazioni') {
 }
 
 //---- Sezione sessualità ----//
-if ($text == '/sessualita') {
+if ($text == '/sessualita') { //01
   header("Content-Type: application/json");
   $answer = "Scegli un argomento specifico tra quelli proposti:
   /preservativo_rotto - Si è rotto il preservativo?
@@ -47,7 +47,7 @@ if ($text == '/sessualita') {
   echo json_encode($parameters);
 }
 
-if ($text == '/preservativo_rotto') {
+if ($text == '/preservativo_rotto') { //02
   header("Content-Type: application/json");
   $answer = "Sei interessato ad avere informazioni sulla contraccezione d’emergenza o sulle infezioni sessualmente trasmesse?
   /gravidanza_indesiderata
@@ -57,7 +57,7 @@ if ($text == '/preservativo_rotto') {
   echo json_encode($parameters);
 }
 
-if ($text == '/gravidanza_indesiderata') {
+if ($text == '/gravidanza_indesiderata') { //03
   header("Content-Type: application/json");
   $answer = "Si è rotto o non hai usato il preservativo?
 Dato che volevi usare il preservativo o sei preoccupato/a di non averlo usato vuol dire che non è tua intenzione affrontare una gravidanza. A questo punto è opportuno ricorrere alla contraccezione d’emergenza. Sei maggiorenne o minorenne?
@@ -71,7 +71,7 @@ Vuoi informazioni sulla contraccezione per non correre il rischio di una gravida
   echo json_encode($parameters);
 }
 
-if ($text == '/maggiorenne') {
+if ($text == '/maggiorenne') { //04
   header("Content-Type: application/json");
   $answer = "In questo caso puoi andare direttamente in farmacia e acquistare la pillola del giorno dopo.
 Puoi anche rivolgerti al consultorio della tua zona. 
@@ -82,7 +82,7 @@ Puoi anche rivolgerti al consultorio della tua zona.
   echo json_encode($parameters);
 }
 
-if ($text == '/minorenne') {
+if ($text == '/minorenne') { //05
   header("Content-Type: application/json");
   $answer = "In questo caso puoi rivolgerti al consultorio o al tuo medico di famiglia per farti prescrivere la pillola del giorno dopo. Ti invitiamo inoltre a rivolgerti al consultorio della tua zona per adottare un programma contraccettivo più sicuro. 
 /consultori - Vuoi vedere un elenco dei consultori? 
@@ -92,7 +92,7 @@ if ($text == '/minorenne') {
   echo json_encode($parameters);
 }
 
-if ($text == '/consultori') {
+if ($text == '/consultori') { //06
   header("Content-Type: application/json");
   $answer = "Scarica il file:
   https://www.regione.lombardia.it/wps/wcm/connect/f5e7b712-254f-4095-a30d-d895fa020b74/CF+PER+SITO+3_06_19.xlsx";
@@ -101,7 +101,7 @@ if ($text == '/consultori') {
   echo json_encode($parameters);
 }
 
-if ($text == '/cpc') {
+if ($text == '/cpc') { //07
   header("Content-Type: application/json");
   $answer = "Contraccezione d’emergenza. Apri i seguenti link:
 http://www.netyx.it/sesso/contraccettivi/cpc-o-pillola-del-giorno-dopo/
@@ -111,7 +111,7 @@ http://www.salute.gov.it/portale/donna/dettaglioContenutiDonna.jsp?lingua=italia
   echo json_encode($parameters);
 }
 
-if ($text == '/no_preservativo') {
+if ($text == '/no_preservativo') { //08
   header("Content-Type: application/json");
   $answer = "Scegli un comando:
   /gravidanza_indesiderata - se non hai usato il preservativo e sei preoccupato/a per una gravidanza indesiderata
@@ -121,7 +121,7 @@ if ($text == '/no_preservativo') {
   echo json_encode($parameters);
 }
 
-if ($text == '/ist') {
+if ($text == '/ist') { //09
   header("Content-Type: application/json");
   $answer = "Vuoi saperne di più sulle infezioni sessualmente trasmissibili?
 http://www.netyx.it/sesso/malattie-a-trasmissione-sessuale-2/
@@ -138,7 +138,7 @@ iOS: https://apps.apple.com/it/app/smart-sex/id878193172";
   echo json_encode($parameters);
 }
 
-if ($text == '/contraccezione') {
+if ($text == '/contraccezione') { //10
   header("Content-Type: application/json");
   $answer = "Ecco un link con le principali modalità di contraccezione che potrai valutare insieme al tuo ginecologo
   http://www.netyx.it/sesso/contraccettivi/";
@@ -147,20 +147,20 @@ if ($text == '/contraccezione') {
   echo json_encode($parameters);
 }
 
-if ($text == '/approfondimento') {
+if ($text == '/approfondimento') { //11
   header("Content-Type: application/json");
   $answer = "Quale di questa tematica vorresti approfondire?
   /definizione - Ti interessa sapere cos’è la sessualità?
   /relazioni – Hai una relazione o ne vorresti una e ti interessa approfondire il tema?
   /corpo – Come funzionano i miei organi sessuali? Perché il mio corpo è cambiato? Come faccio se non mi piace?
-  /identitàdigenere – cos’è l’identità di genere?
+  /identitadigenere – cos’è l’identità di genere?
   /piacere – Considerando che la sessualità è un piacere, vuoi approfondire l’argomento?";
   $parameters = array('chat_id' => $chatId, "text" => $answer);
   $parameters["method"] = "sendMessage";
   echo json_encode($parameters);
 }
 
-if ($text == '/definizione') {
+if ($text == '/definizione') { //12
   header("Content-Type: application/json");
   $answer = "Puoi trovare una difinizione a questo link:
   http://www.netyx.it/sesso/sessualita/
@@ -171,7 +171,7 @@ if ($text == '/definizione') {
   echo json_encode($parameters);
 }
 
-if ($text == '/relazioni') {
+if ($text == '/relazioni') { //13
   header("Content-Type: application/json");
   $answer = "/partner - Ti senti spaesato in questo oceano chiamato “relazioni con un partner” e vorresti qualche consiglio da un esperto?
   /orientamento – Vuoi sapere qualcosa in più sull’orientamento sessuale?
@@ -182,18 +182,7 @@ if ($text == '/relazioni') {
   echo json_encode($parameters);
 }
 
-if ($text == '/relazioni') {
-  header("Content-Type: application/json");
-  $answer = "/partner - Ti senti spaesato in questo oceano chiamato “relazioni con un partner” e vorresti qualche consiglio da un esperto?
-  /orientamento – Vuoi sapere qualcosa in più sull’orientamento sessuale?
-  /starbenesoli - Stai bene anche se non hai una relazione e ti domandi il perché?
-  /amarebene – Cosa significa amare bene? Come riconoscere un amore violento?";
-  $parameters = array('chat_id' => $chatId, "text" => $answer);
-  $parameters["method"] = "sendMessage";
-  echo json_encode($parameters);
-}
-
-if ($text == '/partner') {
+if ($text == '/partner') { //14
   header("Content-Type: application/json");
   $answer = "Prova a cliccare questo link:
   http://www.netyx.it/relazioni/sto-con-te-2/";
@@ -202,7 +191,7 @@ if ($text == '/partner') {
   echo json_encode($parameters);
 }
 
-if ($text == '/orientamento') {
+if ($text == '/orientamento') { //15
   header("Content-Type: application/json");
   $answer = "Prova a scaricare questo file:
   https://socialmi.ats-milano.it/165/social/wiki/p69mZJ2zqu9otl_21112019_genereorientamento.pdf";
@@ -212,7 +201,7 @@ if ($text == '/orientamento') {
 }
 
 
-if ($text == '/starbenesoli') {
+if ($text == '/starbenesoli') { //16
   header("Content-Type: application/json");
   $answer = "Non preoccuparti, molto probabilmente hai imparato ad essere indipendente e hai capito che la relazione con il partner è qualcosa di bello, ma che non è obbligatorio.
   Se però provi fastidio quando ti relazioni con gli altri, o preferisci stare sempre da solo, prova a parlarne con qualcuno.";
@@ -221,7 +210,7 @@ if ($text == '/starbenesoli') {
   echo json_encode($parameters);
 }
 
-if ($text == '/amarebene') {
+if ($text == '/amarebene') { //17
   header("Content-Type: application/json");
   $answer = "Prova a dare un'occhiata a questi link:
   http://www.netyx.it/questo-e-amore/
@@ -231,7 +220,7 @@ if ($text == '/amarebene') {
   echo json_encode($parameters);
 }
 
-if ($text == '/corpo') {
+if ($text == '/corpo') { //18
   header("Content-Type: application/json");
   $answer = "/nonmipiaccio - Non ti senti a tuo agio col tuo corpo e non ti piaci?
   /cambiamento - Il tuo corpo è cambiato negli ultimi anni? Vuoi capirci di più?";
@@ -240,7 +229,7 @@ if ($text == '/corpo') {
   echo json_encode($parameters);
 }
 
-if ($text == '/nonmipiaccio') {
+if ($text == '/nonmipiaccio') { //19
   header("Content-Type: application/json");
   $answer = "Guarda questo link:
   http://www.netyx.it/mi-piaccio-io-e-il-mio-corpo/quando-non-mi-piaccio/
@@ -251,7 +240,7 @@ if ($text == '/nonmipiaccio') {
   echo json_encode($parameters);
 }
 
-if ($text == '/cambiamento') {
+if ($text == '/cambiamento') { //20
   header("Content-Type: application/json");
   $answer = "/ragazza - se sei una ragazza
   /ragazzo - se sei un ragazzo";
@@ -260,6 +249,109 @@ if ($text == '/cambiamento') {
   echo json_encode($parameters);
 }
 
+if ($text == '/ragazza') { //21
+  header("Content-Type: application/json");
+  $answer = "http://www.netyx.it/wordpress/wp-content/uploads/2015/09/PUBERTA-FEMMINILE-Brochure.pdf";
+  $parameters = array('chat_id' => $chatId, "text" => $answer);
+  $parameters["method"] = "sendMessage";
+  echo json_encode($parameters);
+}
+
+if ($text == '/ragazzo') { //22
+  header("Content-Type: application/json");
+  $answer = "Scarica il file:
+  http://www.netyx.it/wordpress/wp-content/uploads/2016/05/PUBERTA-MASCHILE-Brochure.pdf
+  Oppure:
+  http://www.netyx.it/andrologia/";
+  $parameters = array('chat_id' => $chatId, "text" => $answer);
+  $parameters["method"] = "sendMessage";
+  echo json_encode($parameters);
+}
+
+if ($text == '/identitadigenere') { //23
+  header("Content-Type: application/json");
+  $answer = "Scarica il file:
+  https://socialmi.ats-milano.it/165/social/wiki/p69mZJ2zqu9otl_21112019_genereorientamento.pdf";
+  $parameters = array('chat_id' => $chatId, "text" => $answer);
+  $parameters["method"] = "sendMessage";
+  echo json_encode($parameters);
+}
+
+if ($text == '/piacere') { //24
+  header("Content-Type: application/json");
+  $answer = "Non ti dimenticare che la sessualità è anche divertimento e piacere. Se non ti diverti e non ti senti bene, forse hai una relazione poco adatta a te o non stai facendo la cosa giusta. Un buon metro di paragone può essere come ti senti la mattina appena sveglio/a dopo essere stato/a con lui/lei.
+  /masturbazione – vuoi approfondire tale tema?
+  /orgasmo – vuoi saperne di più?
+  /desiderio – vuoi conoscere qualcosa in più rispetto al desiderio sessuale?
+  /zoneerogene – quali? Come stimolarle?
+  /comedarepiacere – come dare piacere agli organi sessuali?";
+  $parameters = array('chat_id' => $chatId, "text" => $answer);
+  $parameters["method"] = "sendMessage";
+  echo json_encode($parameters);
+}
+
+if ($text == '/masturbazione') { //25
+  header("Content-Type: application/json");
+  $answer = "Il piacere può derivare anche dalla masturbazione. Non sentirti in colpa, non c’è nulla di sbagliato, anche se spesso ci viene detto così. Trova un luogo privato e tranquillo e scopri il tuo corpo. Ti piacerà!";
+  $parameters = array('chat_id' => $chatId, "text" => $answer);
+  $parameters["method"] = "sendMessage";
+  echo json_encode($parameters);
+}
+
+if ($text == '/orgasmo') { //26
+  header("Content-Type: application/json");
+  $answer = "L’orgasmo è piacere allo stato puro. Ciò che si vive durante questo momento non è uguale per tutti, ma si riferisce ad una gamma di sensazioni, emozioni e risposte corporee variegate";
+  $parameters = array('chat_id' => $chatId, "text" => $answer);
+  $parameters["method"] = "sendMessage";
+  echo json_encode($parameters);
+}
+
+if ($text == '/desiderio') { //27
+  header("Content-Type: application/json");
+  $answer = "Il desiderio è la prima fase del ciclo di risposta sessuale. È il punto di partenza, è il motore che guida il nostro comportamento e la ricerca dell’altro. È reciproco ed è ciò che definisce quando una relazione sessuale è ricercata e voluta, e non imposta.
+  ATTENZIONE perché a volte il desiderio non è reciproco, per questo è necessario che sia chiaro il CONSENSO. 
+  Guarda questo video animato per capire di cosa si tratta: 
+  https://www.youtube.com/watch?v=RhaZDVcGo-o";
+  $parameters = array('chat_id' => $chatId, "text" => $answer);
+  $parameters["method"] = "sendMessage";
+  echo json_encode($parameters);
+}
+
+if ($text == '/zoneerogene') { //28
+  header("Content-Type: application/json");
+  $answer = "Una zona erogena è una parte del nostro corpo che, se sottoposta a stimolazione, produce piacere. Queste zone non sono uguali per tutti. Per scoprire quali sono gioca insieme al tuo partner e scoprilo, oppure chiediglielo. Potreste scoprire insieme che baciarsi il lobo dell’orecchio o il collo può essere molto piacevole.";
+  $parameters = array('chat_id' => $chatId, "text" => $answer);
+  $parameters["method"] = "sendMessage";
+  echo json_encode($parameters);
+}
+
+if ($text == '/comedarepiacere') { //29
+  header("Content-Type: application/json");
+  $answer = "Probabilmente sei curioso di sapere come procurare piacere al tuo o alla tua partner o come procurartelo da solo/a. Posso aiutarti! Devi solo dirmi se vuoi saperne di più sul piacere maschile o sul piacere femminile:
+  /piaceremaschile
+  /piacere femminile";
+  $parameters = array('chat_id' => $chatId, "text" => $answer);
+  $parameters["method"] = "sendMessage";
+  echo json_encode($parameters);
+}
+
+if ($text == '/piaceremaschile') { //30
+  header("Content-Type: application/json");
+  $answer = "Scarica il file:
+  https://socialmi.ats-milano.it/165/social/wiki/C5BCxoXmoYVwis_21112019_comedarepiaceremaschile.pdf";
+  $parameters = array('chat_id' => $chatId, "text" => $answer);
+  $parameters["method"] = "sendMessage";
+  echo json_encode($parameters);
+}
+
+if ($text == '/piacerefemminile') { //31
+  header("Content-Type: application/json");
+  $answer = "Scarica il file:
+  https://socialmi.ats-milano.it/165/social/wiki/ep2vQSije6abBk_21112019_comedarepiacerefemminile.pdf";
+  $parameters = array('chat_id' => $chatId, "text" => $answer);
+  $parameters["method"] = "sendMessage";
+  echo json_encode($parameters);
+}
 
 //---- fine sezione sessualità ----//
 
@@ -280,9 +372,6 @@ if ($text == '/contraccezione') {
   $parameters["method"] = "sendMessage";
   echo json_encode($parameters);
 }
-
-
-
 
 if ($text == '/foodgame') {
   header("Content-Type: application/json");
